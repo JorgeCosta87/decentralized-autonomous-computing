0983913978b44099c54d3bb2fa66de98caa5cce9
equilibrium <jorgemdcosta@gmail.com>
2026-01-08 14:46:14 +0000

    simplfy node payment system

--- programs/dac/src/state/node_info.rs
+++ programs/dac/src/state/node_info.rs
@@ -1,4 +1,3 @@
-use crate::errors::ErrorCode;
 use anchor_lang::prelude::*;
 
 #[derive(InitSpace, AnchorSerialize, AnchorDeserialize, Clone, Copy, PartialEq)]
@@ -16,12 +15,6 @@ pub enum NodeStatus {
     Rejected,
 }
 
-#[derive(InitSpace, AnchorSerialize, AnchorDeserialize, Clone, Copy)]
-pub struct RewardEntry {
-    pub amount: u64,
-    pub slot: u64,
-}
-
 #[account]
 #[derive(InitSpace)]
 pub struct NodeInfo {
@@ -34,28 +27,7 @@ pub struct NodeInfo {
     pub code_measurement: Option<[u8; 32]>,
     pub tee_signing_pubkey: Option<Pubkey>,
     pub node_treasury: Pubkey,
-    #[max_len(64)]
-    pub recent_rewards: Vec<RewardEntry>,
     pub total_earned: u64,
-    pub max_entries_before_transfer: u64,
-    pub last_transfer_slot: u64,
     pub total_tasks_completed: u64,
     pub bump: u8,
-}
-
-impl NodeInfo {
-    pub fn add_reward(&mut self, amount: u64, slot: u64) -> Result<()> {
-        require!(self.recent_rewards.len() < 64, ErrorCode::RewardVectorFull);
-
-        self.recent_rewards.push(RewardEntry { amount, slot });
-        Ok(())
-    }
-
-    pub fn clear_rewards(&mut self) {
-        self.recent_rewards.clear();
-    }
-
-    pub fn total_pending_rewards(&self) -> u64 {
-        self.recent_rewards.iter().map(|r| r.amount).sum()
-    }
 }
